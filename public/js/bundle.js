2e71603de0bd47c14cc63e6e02a6e2c409bc6227
chandan3966 <chandan3966.official@gmail.com>
2023-12-30 02:02:56 -0500

    css fix

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5733,7 +5733,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53123" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63811" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
